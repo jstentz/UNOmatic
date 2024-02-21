8f5082154e98c7b8c388d728231bc37522ed6c95
David Peng <dzp@andrew.cmu.edu>
2024-02-21 01:19:37 -0500

    test continuous servo and lay out event loop with some empty functions

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -1,13 +1,21 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
 void setup() {
   Serial.begin(9600);
+  myservo.attach(10);  // attaches the servo on pin 10 to the servo object
 }
-
 
 void loop() {
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
-    Serial.print("echo: ");
     Serial.println(data);
+    int value = data.toInt();
+    myservo.write(value);
   }
-  delay(100);
+  delay(10);
 }
--- rpi/main.py
+++ rpi/main.py
@@ -1,43 +1,91 @@
 from picamera2 import Picamera2
+from gpiozero import LED, Button
 import serial
 import time
+import numpy as np
 
 def uart_init():
     ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
     return ser
 
-def cam_init():
-    picam2 = Picamera2(0)
-    config = picam2.create_still_configuration({"size": (1280, 720)})
-    picam2.start(show_preview=True)
-    return picam2, config
+def cam_init(n, res):
+    cam = Picamera2(n)
+    config = cam.create_still_configuration({"size": res})
+    cam.configure(config)
+    cam.start(show_preview=True)
+    return cam
+
 
 def get_line(ser):
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode("ascii").strip()
             return line
+        time.sleep(0.1)
 
+def model_init():
+    pass
+
+def classify(model, image):
+    pass
+
+def play_one_turn(card):
+    pass
+
+def add_card(card):
+    pass
+
+def rotate(ser, angle):
+    ser.write(f'r: {angle}')
+    while True:
+        if ser.in_waiting > 0:
+            return
+        time.sleep(0.1)
+
+def deal(ser, cam, num_deal):
+    images = []
+    for _ in range(num_deal):
+        images.append(cam.capture_array())
+        ser.write(f'd')
+        while True:
+            if ser.in_waiting > 0:
+                break
+            time.sleep(0.1)
+    return images
+            
+def event_loop():
+    ser = uart_init()
+    cam_top = cam_init(0, (540, 360))
+    cam_bottom= cam_init(0, (540, 360))
+    cam_top.start(show_preview=True)
+    cam_bottom.start(show_preview=True)
+    model = model_init()
+    button_new_move = Button(2)
+    while True:
+        button_new_move.wait_for_press()
+        out_top = cam_top.capture_array()
+        card = classify(model, out_top)
+        num_deal = play_one_turn(card)
+        # need to do a bunch of control flow
+        rotate(ser, 90)
+        images = deal(ser, cam_bottom, num_deal)
+        for image in images:
+            card = classify(model, image)
+            add_card(card)
 
 def main():
     ser = uart_init()
-    picam2, config = cam_init()
+    cam = cam_init(0, (540, 360))
     while True:
         cmd = input(">> ")
         if cmd == "q":
             break
-        # picam2.switch_mode_and_capture_file(config, "image.jpg")
+        cam.capture_file("image.jpg")
         data = cmd + "\n"
         data = data.encode("ascii")
         ser.write(data)
         line = get_line(ser)
         print(line)
-            
-        
-
-
-    
-
 
 if __name__ == "__main__":
     main()
