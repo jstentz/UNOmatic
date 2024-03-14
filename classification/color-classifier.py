b75e93c1a2c1a1d60ffb86c62230be0b649fee0d
Jason Stentz <jstentz@andrew.cmu.edu>
2024-03-13 22:52:50 -0400

    100% best color classification ever

--- classification/color-classifier.py
+++ classification/color-classifier.py
@@ -2,36 +2,82 @@
 Script for finding the color of an image.
 '''
 
-import pandas as pd
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2 as cv
-from sklearn.cluster import KMeans
-
-img = cv.imread('top_data/top_318.jpg')
-
-# convert to RGB
-img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-
-# flatten to a single list of vectors
-img = img.reshape((img.shape[1]*img.shape[0],3))
-
-kmeans = KMeans(n_clusters=5)
-s = kmeans.fit(img)
-
-# get the labels of each of the color vectors (which cluster they are assigned to)
-labels = list(kmeans.labels_)
-
-# get the centroid vectors
-centroid = kmeans.cluster_centers_
+import os
 
 
-percent = []
-for i in range(len(centroid)):
-  j = labels.count(i)
-  j = j / len(labels)
-  percent.append(j)
+def calculate_average_intensity(image, mask):
+  masked_image = cv2.bitwise_and(image, image, mask=mask)
+  masked_gray = cv2.cvtColor(masked_image, cv2.COLOR_BGR2GRAY)
+  total_intensity = np.sum(masked_gray)
+  size = image.shape[0] * image.shape[1]
+  average_intensity = total_intensity / size
+  return average_intensity
 
-plt.pie(percent,colors=np.array(centroid/255),labels=np.arange(len(centroid)))
-# plt.imshow(img)
-plt.show()
+def get_color(image):
+
+  # Convert the image from BGR to HSV color space
+  hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+  # Define range for yellow color in HSV
+  lower_yellow = np.array([25, 100, 100])
+  upper_yellow = np.array([35, 255, 255])
+
+  # Create a mask to extract yellow regions
+  yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+
+  # Calculate average intensity after applying yellow mask
+  yellow_intensity = calculate_average_intensity(image, yellow_mask)
+
+  # Define range for red color in HSV
+  lower_red = np.array([0, 100, 100])
+  upper_red = np.array([10, 255, 200])
+
+  # Create a mask to extract red regions
+  red_mask = cv2.inRange(hsv_image, lower_red, upper_red)
+
+  # Calculate average intensity after applying red mask
+  red_intensity = calculate_average_intensity(image, red_mask)
+
+  # Define range for blue color in HSV
+  lower_blue = np.array([90, 100, 100])
+  upper_blue = np.array([150, 255, 255])
+
+  # Create a mask to extract blue regions
+  blue_mask = cv2.inRange(hsv_image, lower_blue, upper_blue)
+
+  # Calculate average intensity after applying blue mask
+  blue_intensity = calculate_average_intensity(image, blue_mask)
+
+  # Define range for red color in HSV
+  lower_green = np.array([40, 100, 100])
+  upper_green = np.array([80, 255, 255])
+
+  # Create a mask to extract green regions
+  green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
+
+  # Calculate average intensity after applying green mask
+  green_intensity = calculate_average_intensity(image, green_mask)
+
+  intensities = [red_intensity, green_intensity, blue_intensity, yellow_intensity]
+  labels = ['red', 'green', 'blue', 'yellow']
+  label = max(zip(intensities, labels), key=lambda x: x[0])[1]
+  return label
+
+
+if __name__ == '__main__':
+  images_dir = 'top_data/'
+  image_names = os.listdir(images_dir)
+
+
+  for image_name in sorted(image_names, key=lambda x: int(x.removesuffix('.jpg')[x.find('_')+1:])):
+    image_path = os.path.join(images_dir, image_name)
+
+    # Read the image
+    image = cv2.imread(image_path)
+    label = get_color(image)
+
+    cv2.imshow(label, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
