b84955778958bfa646963e1d6d3e07ddcac224c0
Jason Stentz <jstentz@andrew.cmu.edu>
2024-04-19 20:11:44 -0400

    fixed game over classification

--- uno/manager.py
+++ uno/manager.py
@@ -24,7 +24,7 @@ class Manager:
   # TODO: add info about state correction from displayers
   TO_CONTROLLER = [GoNextPlayer, DealCard, GetUserInput]
   TO_STATE = [PlayCard, DealtCard, SkipTurn, SetColor, Bluff, CallUNO, UNOFail, CorrectedState]
-  TO_DISPLAYERS = [CurrentState, GameOver, RoundOver]
+  TO_DISPLAYERS = [CurrentState]
 
   def __init__(self, controller_type: type[Controller], displayer_types: Collection[type[Displayer]], logger: logging.Logger) -> None:
     # make all of the queues for communication
@@ -41,6 +41,22 @@ class Manager:
     self.displayers = [displayer_type(displayer_queue, self.manager_queue) for displayer_type, displayer_queue in zip(displayer_types, self.displayer_queues)] # TODO: change this
     self.logger.info(f'Initialized {", ".join([displayer_type.__name__ for displayer_type in displayer_types])}')
 
+  def clear_queues(self):
+    # clear everyone's queues
+    queues_to_clear = [self.manager_queue, self.controller_queue, self.state_queue] + self.displayer_queues
+    for queue in queues_to_clear:
+      while not queue.empty():
+        queue.get()
+
+  def reset_controller(self):
+    self.controller.reset()
+
+  def reset_state(self, request: Request):
+    self.state.reset(request)
+
+  def reset_displayers(self):
+    for displayer in self.displayers:
+      displayer.reset()
 
   def start(self):
     self.controller.start()
@@ -57,17 +73,17 @@ class Manager:
       self.logger.info(f'Manager received {request}')
 
       if type(request) in [Reset, RoundReset]:
-        # clear everyone's queues
-        queues_to_clear = [self.manager_queue, self.controller_queue, self.state_queue] + self.displayer_queues
-        for queue in queues_to_clear:
-          while not queue.empty():
-            queue.get()
-        
-        self.controller.reset()
-        self.state.reset(request)
+        self.clear_queues()
+        self.reset_controller()
+        self.reset_state(request)
+        self.reset_displayers()
 
-        for displayer in self.displayers:
-          displayer.reset()
+      elif type(request) in [GameOver, RoundOver]:
+        self.clear_queues()
+        self.reset_controller()
+        
+        for displayer_queue in self.displayer_queues:
+          displayer_queue.put(request)
 
       elif type(request) in Manager.TO_CONTROLLER:
         self.controller_queue.put(request)
